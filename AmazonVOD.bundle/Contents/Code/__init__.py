fdd0ffd7d8c41d50fb0ee36d353123c0cd43273e
David Bartle <captindave@gmail.com>
2009-07-24 09:25:42 -0700

    Refactored the login process after updating preferences.  Also fixed a bug in the "Your Purchases" menu where AppendItem was used instead of Append

--- AmazonVOD.bundle/Contents/Code/__init__.py
+++ AmazonVOD.bundle/Contents/Code/__init__.py
@@ -12,7 +12,6 @@ from PMS.Shortcuts import *
 PLUGIN_PREFIX     = "/video/AmazonVOD"
 PREFS_PREFIX      = "%s/prefs||Amazon Preferences/" % PLUGIN_PREFIX
 
-
 AMAZON_PROXY_URL            = "http://atv-sr.amazon.com/proxy/proxy"
 AMAZON_SEARCH_URL           = "http://www.amazon.com/s/"
 AMAZON_PRODUCT_URL          = "http://www.amazon.com/gp/product/%s"
@@ -20,7 +19,6 @@ AMAZON_PRODUCT_URL          = "http://www.amazon.com/gp/product/%s"
 CACHE_INTERVAL              = 3600
 DEBUG                       = True
 
-global __customerId, __token, __tokensChecked
 __customerId = None
 __token      = None
 __tokensChecked = False
@@ -29,15 +27,39 @@ __tokensChecked = False
 
 def Start():
   Plugin.AddPrefixHandler(PLUGIN_PREFIX, Menu, L("amazon"), "icon-default.png", "art-default.png")
+  Plugin.AddPrefixHandler("%s/:/prefs/set" % PLUGIN_PREFIX ,PrefsHandler, "phandler")
   Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
   Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 
 def CreatePrefs():
   Prefs.Add(id='login', type='text', default='', label='Login Email')
   Prefs.Add(id='password', type='text', default='', label='Password', option='hidden')
-  
-def Menu():
+
+def PrefsHandler(login=None,password=None):
+  global __customerId, __token, __tokensChecked
+  __customerId = None
+  __token = None
+  __tokensChecked = False
+
+
+  message_add = ""
+  if login != None and password != None:
+      cid,tok  = streamingTokens()
+      if cid and tok:
+        message_add = "Login to Amazon OK"
+      else:
+        message_add = "Could not log into Amazon"
+
+  title = "Preferences Updated"
+  message = "Amazon preferences updated."
+  dir = MessageContainer(title,"%s\n%s" % (message,message_add))
+  return dir
+
+def Menu(message_title=None,message_text=None):
+  customerId, token = streamingTokens()
   dir = MediaContainer()
+  if message_title != None and message_text != None:
+    dir.SetMessage(message_title,message_text)
   if Prefs.Get('login'):
     dir.Append(Function(DirectoryItem(MenuYourPurchases,"Your Purchases")))
   dir.Append(Function(SearchDirectoryItem(MenuSearch,"Search", "Search", R("search.png"))))
@@ -48,11 +70,10 @@ def MenuYourPurchases(sender):
   dir = MediaContainer(title2=sender.itemTitle)
   purchasedAsinList = purchasedAsin()
   for i in makeDirItemsFromAsin(purchasedAsinList):
-    dir.AppendItem(i)
+    dir.Append(i)
   return dir
 
 def MenuSearch(sender, query=None):
-  PMS.Log("MenuSearch query: %s" % repr(query))
   dir = MediaContainer(title2=sender.itemTitle)
   res = asin_search(query)
   items = makeDirItemsFromAsin(res)
@@ -64,8 +85,6 @@ def MenuSearch(sender, query=None):
 
 def MenuSeasonList(sender, asin=None):
   dir = MediaContainer(title2=sender.itemTitle)
-
-  PMS.Log('seasonlist for %s' % asin)
 
   url = AMAZON_PRODUCT_URL % asin
 
@@ -84,60 +103,24 @@ def MenuSeasonList(sender, asin=None):
 
 ####################################################################################################
 
-def makeDirItemsFromAsin(items):
-
-  ret = []
-
-  for asin in items:
-    #PMS.Log(repr(asin))
-    thumb = asin.get('IMAGE_URL_LARGE',asin.get('IMAGE_URL_SMALL',''))
-    desc = asin.get('SYNOPSIS','')
-    rating = float(asin.get('AMAZONRATINGS',0.0))
-
-    if 'EPISODENUMBER' in asin and 'SEASONNUMBER' in asin:
-      title = 'S%02dE%02d : %s' % (int(asin['SEASONNUMBER']),int(asin['EPISODENUMBER']),asin['TITLE'])
-    else:
-      title = asin.get('TITLE','')
-
-    url = AMAZON_PRODUCT_URL % asin['ASIN']
-    PMS.Log("%s" % url)
-    duration = int(asin.get('RUNTIME',0))*60*1000
-
-    stream_url = asin.get('STREAM_URL_1','')
-
-    if stream_url != '':
-      other_args = dict()
-      if 'SEASONNUMBER' in asin:
-        other_args['season'] = asin['SEASONNUMBER']
-      if 'EPISODENUMBER' in asin:
-        other_args['episode'] = asin['EPISODENUMBER']
-      item = WebVideoItem(url,title,summary=desc,duration=str(duration),thumb=thumb,rating=str(rating),**other_args)
-    else:
-      item = Function(DirectoryItem(MenuSeasonList,"%s" % title),asin=asin['ASIN'])
-
-    ret.append(item)
-
-  ret.sort(cmp=lambda a,b: cmp(a.__dict__.get('title'),b.__dict__.get('title')))
-  return ret
-
 
 def signIn():
 
-  PMS.Log("signIn() called")
+  #PMS.Log("signIn() called")
 
-  USER = Prefs.Get("loginemail")
+  USER = Prefs.Get("login")
   PASS = Prefs.Get("password")
 
   if not (USER and PASS):
     return False
 
-  x = HTTP.Get('https://www.amazon.com/gp/sign-in.html')
+  x = HTTP.Request('https://www.amazon.com/gp/sign-in.html', errors='replace')
 
   sessId = None
   for idx,cookie in enumerate(HTTP.__cookieJar):
     if cookie.name == 'session-id':
       sessId = cookie.value
-
+ 
   if not sessId:
       return False
 
@@ -150,26 +133,27 @@ def signIn():
       'password': PASS,
       'email': USER
   }
-  x = HTTP.Request('https://www.amazon.com/gp/flex/sign-in/select.html',values=params,encoding='utf-8')
+  x = HTTP.Request('https://www.amazon.com/gp/flex/sign-in/select.html',values=params,errors='replace')
 
   return True
 
+####################################################################################################
+
 def streamingTokens():
   global __customerId, __token, __tokensChecked
-
 
   if (__customerId and __token) or __tokensChecked:
       return (__customerId,__token)
 
   __tokensChecked = True
 
-  html = HTTP.Get('http://www.amazon.com/gp/video/streaming/')
+  html = HTTP.Request('http://www.amazon.com/gp/video/streaming/',errors='replace')
   paramStart = html.find("&customer=")
   if paramStart == -1:
       ret = signIn()
       if not ret:
         return (None,None)
-      html = HTTP.Get('http://www.amazon.com/gp/video/streaming/')
+      html = HTTP.Request('http://www.amazon.com/gp/video/streaming/',errors='replace')
       paramStart = html.find("&customer=")
       if paramStart == -1:
           return (None,None)
@@ -192,14 +176,14 @@ def purchasedAsin():
   if not (customerId and token):
     return ret
 
-  # http://atv-sr.amazon.com/proxy/proxy?c=A2UADYTSXK548T&f=getQueue&token=af2c81f6854618a5a1b72fe206d674a2&t=Streaming
+  # http://atv-sr.amazon.com/proxy/proxy?c=<customer id>&f=getQueue&token=<token>&t=Streaming
   params = {
     'c': customerId,
     'token': token,
     'f': 'getQueue',
     't': 'Streaming'
   }
-  html = HTTP.Request(AMAZON_PROXY_URL,values=params,encoding='utf-8')
+  html = HTTP.Request(AMAZON_PROXY_URL,values=params,errors='replace')
   jsonString = html.split("\n")[2]
   for i in JSON.ObjectFromString(jsonString):
     asinInfo = i.get('FeedAttributeMap',None)
@@ -207,59 +191,6 @@ def purchasedAsin():
       ret.append(asinInfo)
 
   return ret
-
-def HandleRequest(pathNouns, count):
-  customerId, token = streamingTokens()
-
-  if pathNouns[0].startswith("prefs"):
-    dir = MediaContainer()
-    dir.SetAttr("title2","Amazon Preferences")
-
-    loginemail = Prefs.Get("loginemail")
-    password = Prefs.Get("password")
-
-    if count >= 3:
-        dir.SetAttr('replaceParent', '1')
-        field = pathNouns[-2].split("^")[1]
-        Prefs.Set(field, pathNouns[-1])
-        __tokensChecked = False
-        loginemail = Prefs.Get("loginemail")
-        password = Prefs.Get("password")
-
-        message_add = ""
-        if loginemail != None and password != None:
-            __customerId = None
-            __token = None
-            cid,tok  = streamingTokens()
-            if cid and tok:
-              message_add = "Login to Amazon OK"
-            else:
-              message_add = "Could not log into Amazon"
-
-
-        title = "Preferences Updated"
-        if field == 'loginemail':
-            message = "Amazon login email updated."
-        else:
-            message = "Amazon password updated."
-
-        dir.SetMessage(title,"%s\n%s" % (message,message_add))
-
-    if loginemail != None:
-        dir.AppendItem(SearchDirectoryItem(PREFS_PREFIX + "prefs^loginemail", "Change your email login", "Change your email login", ""))
-    else:
-        dir.AppendItem(SearchDirectoryItem(PREFS_PREFIX + "prefs^loginemail", "Set your email login", "Set your email login", ""))
-
-
-    if password != None:
-        dir.AppendItem(SearchDirectoryItem(PREFS_PREFIX + "prefs^password", "Change your password", "Change your password", ""))
-    else:
-        dir.AppendItem(SearchDirectoryItem(PREFS_PREFIX + "prefs^password", "Set your password", "Set your password", ""))
-
-
-    return dir.ToXML();
-
-
 
 def asin_search(query):
 
@@ -301,3 +232,38 @@ def asin_info(asinList):
     if i.get('ISSTREAMABLE','N') == 'Y':
       ret.append(i)
   return ret
+
+def makeDirItemsFromAsin(items):
+
+  ret = []
+
+  for asin in items:
+    thumb = asin.get('IMAGE_URL_LARGE',asin.get('IMAGE_URL_SMALL',''))
+    desc = asin.get('SYNOPSIS','')
+    rating = float(asin.get('AMAZONRATINGS',0.0))
+
+    if 'EPISODENUMBER' in asin and 'SEASONNUMBER' in asin:
+      title = 'S%02dE%02d : %s' % (int(asin['SEASONNUMBER']),int(asin['EPISODENUMBER']),asin['TITLE'])
+    else:
+      title = asin.get('TITLE','')
+
+    url = AMAZON_PRODUCT_URL % asin['ASIN']
+    duration = int(asin.get('RUNTIME',0))*60*1000
+
+    stream_url = asin.get('STREAM_URL_1','')
+
+    if stream_url != '':
+      other_args = dict()
+      if 'SEASONNUMBER' in asin:
+        other_args['season'] = asin['SEASONNUMBER']
+      if 'EPISODENUMBER' in asin:
+        other_args['episode'] = asin['EPISODENUMBER']
+      item = WebVideoItem(url,title,summary=desc,duration=str(duration),thumb=thumb,rating=str(rating),**other_args)
+    else:
+      item = Function(DirectoryItem(MenuSeasonList,"%s" % title),asin=asin['ASIN'])
+
+    ret.append(item)
+
+  ret.sort(cmp=lambda a,b: cmp(a.__dict__.get('title'),b.__dict__.get('title')))
+  return ret
+
